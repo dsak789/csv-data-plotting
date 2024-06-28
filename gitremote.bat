53c43272f076e232792c9c4660ac81d8efeaa9ef
dsak789 <dsak.official@gmail.com>
2024-06-29 01:17:22 +0530

    NODEv2

---
+++ gitremote.bat
@@ -0,0 +1,10 @@
+git status
+git add .
+git commit -m "NODEv2"
+git push origin dsak7
+git pull origin main 
+git checkout main  
+git merge dsak7  
+git push -u origin main
+git checkout dsak7
+exit
